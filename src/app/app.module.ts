d4e23c934aee8f373e61457ad06c3808765fae0f
student <student@example.com>
2018-02-09 17:01:51 +0300

    Начальный комментарий

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { GamePage } from '../pages/game/game';
+import { ScorePage } from '../pages/score/score';
+import { GamemodePage } from '../pages/gamemode/gamemode';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -12,18 +13,20 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    GamePage,
+    ScorePage,
+    GamemodePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+    GamePage,
+    ScorePage,
+    GamemodePage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/gamemode/gamemode.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-gamemode',
+  templateUrl: 'gamemode.html'
+})
+export class GamemodePage {
+
+  constructor(public navCtrl: NavController) {
+  }
+  
+}
